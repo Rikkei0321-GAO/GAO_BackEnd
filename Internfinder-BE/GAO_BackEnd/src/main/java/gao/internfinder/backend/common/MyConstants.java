1ac7e3eb9991dedcf3f1216d07908632cb3a8936
ThanhHaiii <thanhhaiphan111@gmail.com>
2021-04-13 20:37:59 +0700

    forgot-password

---
+++ Internfinder-BE/GAO_BackEnd/src/main/java/gao/internfinder/backend/common/MyConstants.java
@@ -0,0 +1,12 @@
+package gao.internfinder.backend.common;
+
+public class MyConstants {
+    // Replace with your email here:
+    public static final String MY_EMAIL = "thanhhai23399@gmail.com";
+
+    // Replace password!!
+    public static final String MY_PASSWORD = "thanhcong";
+
+    // And receiver!
+    public static final String MAIL_RECEIVER= "";
+}
