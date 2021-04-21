1ba5e9ebc7ff8f702b292a5f53d6ab51c3d5ef4a
datpham92qn <80908898+datpham92qn@users.noreply.github.com>
2021-04-22 00:12:47 +0700

    Revert "forgot-password"

--- Internfinder-BE/GAO_BackEnd/src/main/java/gao/internfinder/backend/repository/AccountRepository.java
+++ Internfinder-BE/GAO_BackEnd/src/main/java/gao/internfinder/backend/repository/AccountRepository.java
@@ -4,36 +4,15 @@ package gao.internfinder.backend.repository;
 
 import gao.internfinder.backend.Entity.Account;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
 @Repository
-@Transactional
 public interface AccountRepository extends JpaRepository<Account, Integer> {
 	Optional<Account> findByUsername(String username);
 
 	Boolean existsByUsername(String username);
 
 	Boolean existsByEmail(String email);
-
-	@Query(value = "SELECT email FROM account where email= ?1", nativeQuery = true)
-	String existsByEmails(String email);
-
-	@Query(value = "select * from account where verification_code =?1",nativeQuery = true)
-	Account findAccountByVerificationCode(String verifyCode);
-
-	@Modifying
-	@Query(value ="update account set verification_code=?1 where email =?2",nativeQuery = true)
-	void addVerificationCode(String code,String email);
-
-	@Modifying
-	@Query(value = "update account set password =?1,verification_code=null where verification_code=?2 ",nativeQuery = true)
-	void saveNewPassword(String password, String code);
-
-
-
 }
