0ddfb0014bf557a9011549530ed2798851428215
tranhoanganh <hoanganhtran669@gmail.com>
2021-05-10 09:49:11 +0700

    create_cv

--- Internfinder-BE/GAO_BackEnd/src/main/java/gao/internfinder/backend/Service/ICreateCV.java
+++ Internfinder-BE/GAO_BackEnd/src/main/java/gao/internfinder/backend/Service/ICreateCV.java
@@ -1,7 +1,10 @@
 package gao.internfinder.backend.Service;
 
 import gao.internfinder.backend.dto.CVDTO;
+import gao.internfinder.backend.dto.DataCVDTO;
+import gao.internfinder.backend.dto.User;
 
 public interface ICreateCV {
     void addCV(CVDTO cv);
+    User postdata(DataCVDTO cv);
 }
